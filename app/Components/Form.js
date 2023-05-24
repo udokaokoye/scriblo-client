4771f7bc404b412ff3fa5e78f4f4c8264d269511
udokaokoye <leviokoye.com>
2023-05-24 01:36:43 -0400

    implementing waitlist API

--- app/Components/Form.js
+++ app/Components/Form.js
@@ -2,24 +2,52 @@
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import React from 'react'
 import Button from './Button';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Link from 'next/link';
 
 function Form() {
     const [email, setemail] = useState('')
     const [message, setmessage] = useState('')
+    const [errormessage, seterrormessage] = useState('')
+
+    useEffect(() => {
+      if (message !== '') {
+        setTimeout(() => {
+          setmessage('')
+        }, 15000);
+      }
+    }, [message])
+    
 
     const handleSubmit = () => {
-
-
-        setmessage("🎉 you've officially become a part of the coolest gang in the blogosphere By joining our waitlist! 🎉")
+      if (email === '') {
+        seterrormessage('please enter your email')
+        return;
+      }
+      const formData = new FormData();
+      formData.append('email', email);
+      seterrormessage('')
+      fetch('https://api.myscriblo.com/waitlist/index.php', {
+        method: 'POST',
+        body: formData,
+      }).then((response) => response.json()).then((data) => {
+        // console.log(data)
+        if (data.status == 200) {
+          setmessage("🎉 Thanks for joining 🎉")
+        } else {
+          setmessage("😢 something went wrong, please try again later")
+        }
+      }).catch((error) => {
+        console.error('Error:', error);
+      });
     }
   return (
     <div>
         <div className="joinWaitlist">
         <h1>Join the wait list</h1>
         <p>Get notified when we launch.</p>
-        {message && (<p className='message'>{message}</p>)}
+        {message && (<div className='message'><p className='message_bold'>{message}</p> <p className='message_small'>you've officially become a part of the coolest gang in the blogosphere By joining our waitlist! 🌚</p></div>)}
+        {errormessage && (<p className='errMsg'>{errormessage}</p>)}
         <input value={email} onChange={(text) => setemail(text.target.value)} type="email" placeholder="example@mail.com" /><br />
 
         <Button title={"Join"} onClick={() => handleSubmit()} />
