7941c5250127e6ff260ba424e9b6cd5dd1318e34
udokaokoye <leviokoye.com>
2023-05-24 09:53:04 -0400

    re arraqnged the files

---
+++ Components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+        <div className='navigation'>
+            <div className='navigation__logo'>Logo</div>
+            <div className='navigation__menu'>
+            
+            </div>
+        </div>
+
+  )
+}
+
+export default Navigation
---
+++ app/signup/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+<div className="signup">
+  <h1>Signup</h1>
+</div>
+  )
+}
+
+export default Signup
