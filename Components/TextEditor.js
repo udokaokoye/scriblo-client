76b3e39302ae3d8a5fa45273557ee679270184fb
udokaokoye <leviokoye.com>
2023-05-30 13:49:17 -0400

    still working on this

--- Components/TextEditor.js
+++ Components/TextEditor.js
@@ -1,46 +1,146 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { EditorState, ContentState, AtomicBlockUtils, convertToRaw } from 'draft-js';
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
+// import { Edit } from "@mui/icons-material";
 
 function TextEditor({ uploadImages, setuploadImages}) {
-  const [editorState, seteditorState] = useState("");
+  const [editorState, seteditorState] = useState(EditorState.createEmpty());
+  const [content, setcontent] = useState('')
 
-  const onEditorStateChange = (editorState) => {
-    seteditorState(editorState);
-  };
-  const uploadImageCallBack = (file) => {
-           // long story short, every time we upload an image, we
-        // need to save it to the state so we can get it's data
-        // later when we decide what to do with it.
-    
-        // Make sure you have a uploadImages: [] as your default state
+  // useEffect(() => {
+  //   console.log("Content: " + content)
+  // }, [content])
 
-    
-        const imageObject = {
-          file: file,
-          localSrc: URL.createObjectURL(file),
-        }
-    
-        setuploadImages(prevState => [...prevState, imageObject]);
-    
-        // We need to return a promise with the image src
-        // the img src we will use here will be what's needed
-        // to preview it in the browser. This will be different than what
-        // we will see in the index.md file we generate.
-        return new Promise(
-          (resolve, reject) => {
-            resolve({ data: { link: imageObject.localSrc } });
-          }
-        );
+  function getEmbedLink(url) {
+    let embedLink = '';
+
+    if (url.includes('youtube.com') || url.includes('youtu.be')) {
+      // YouTube
+      const videoId = url.match(/(?:\?v=|\/embed\/|\.be\/)([\w\-\_]+)/)[1];
+      embedLink = `https://www.youtube.com/embed/${videoId}`;
+    } else if (url.includes('vimeo.com')) {
+      // Vimeo
+      const videoId = url.match(/vimeo(?:.com\/video\/|\/)(\d+)/)[1];
+      embedLink = `https://player.vimeo.com/video/${videoId}`;
+    } else if (url.includes('twitter.com')) {
+      // Twitter
+      const tweetId = url.match(/\/status\/(\d+)/)[1];
+      embedLink = `https://twitframe.com/show?url=https://twitter.com/anyuser/status/${tweetId}`;
+    } else if (url.includes('facebook.com')) {
+      // Facebook
+      const postId = url.match(/facebook\.com\/([^\/]+)\/posts\/(\d+)/)[2];
+      embedLink = `https://www.facebook.com/plugins/post.php?href=https://www.facebook.com/${postId}`;
+    } else if (url.includes('instagram.com')) {
+      // Instagram
+      const postUrl = url.replace(/(\/\?utm.*)|\/$/, '');
+      embedLink = `https://www.instagram.com/p/${postUrl.split('/').pop()}/embed`;
+    } else {
+      // Unsupported source, return the original URL
+      embedLink = url;
+    }
+  
+    return embedLink;
   }
+
+  const blockRendererFn = (contentBlock) => {
+    if (contentBlock.getType() === 'atomic') {
+      const entityKey = contentBlock.getEntityAt(0);
+      if (entityKey) {
+        const entity = ContentState.getEntity(entityKey);
+        if (entity.getType() === 'iframe') {
+          const data = entity.getData();
+          const { src, className } = data;
+          return {
+            component: CustomIFrameComponent,
+            editable: false,
+            props: {
+              src,
+              className,
+            },
+          };
+        }
+      }
+    }
+    return null;
+  };
+  
+  const CustomIFrameComponent = ({ src, className }) => (
+    <div className={className}>
+      <iframe src={src} title="Embedded Content" />
+    </div>
+  );
+
+
+  const uploadImageCallBack = (file) => {
+    return new Promise((resolve, reject) => {
+        const imageObject = {
+            file: file,
+            localSrc: URL.createObjectURL(file),
+          }
+        // const contentState = editorState.getCurrentContent();
+        // const contentStateWithEntity = contentState.createEntity( 'IMAGE', 'IMMUTABLE', { src: imageObject.localSrc, className: 'uploadImage' } );
+        // const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+        // const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
+        // const newBlock = new ContentBlock({
+        //     key: genKey(),
+        //     type: 'atomic',
+        //     text: ' ',
+        //     characterList: List(),
+        //     data: Map({
+        //       entity: entityKey
+        //     })
+        //   });
+
+        //   const newContentState = contentStateWithEntity.merge({
+        //     blockMap: contentStateWithEntity.getBlockMap().set(newBlock.getKey(), newBlock),
+        //     selectionAfter: new SelectionState({
+        //       anchorKey: newBlock.getKey(),
+        //       anchorOffset: 0,
+        //       focusKey: newBlock.getKey(),
+        //       focusOffset: 0,
+        //       isBackward: false,
+        //       hasFocus: true
+        //     })
+        //   });
+        //   const newEditorStateWithBlock = EditorState.set(newEditorState, {
+        //     currentContent: newContentState
+        //   });
+
+        // seteditorState(newEditorStateWithBlock);
+        
+            setuploadImages(prevState => [...prevState, imageObject]);
+            resolve( { data: { link: imageObject.localSrc } });
+    });
+
+        // return new Promise(
+        //   (resolve, reject) => {
+        //     resolve({ data: { link: imageObject.localSrc } });
+        //   }
+        // );
+  }
+  const embededLinkCallBack = (url) => {
+    const embedLink = getEmbedLink(url);
+    console.log(embedLink)
+
+    return embedLink;
+  }
+  const AtomicImageComponent = (props) => {
+    const { block, contentState } = props;
+    const entity = contentState.getEntity(block.getEntityAt(0));
+    const { src, className } = entity.getData();
+
+    return <img src={src} className={className} alt="uploaded" />;
+  };
   return (
-    <>
+    <div>
       <Editor
         editorState={editorState}
         toolbarClassName="toolbarClassName"
-        wrapperClassName="wrapperClassName"
-        editorClassName="editorClassName"
+        wrapperClassName="editorWrapper"
+        editorClassName="editorMain"
         toolbar={{
           options: [
             "inline",
@@ -60,11 +160,42 @@ function TextEditor({ uploadImages, setuploadImages}) {
           history: { inDropdown: true },
           image: {
             uploadCallback: uploadImageCallBack,
-          }
+            className: "uploadImage", 
+            previewImage: true,
+            alignmentEnabled: false,
+            // alt: { present: false, mandatory: false },
+            defaultSize: {
+                height: '100%',
+                width: '100%',
+                margin: "0 auto",
+
+              },
+          },
+          embedded: {
+            embedCallback: embededLinkCallBack,
+            defaultSize: {
+              height: 'auto',
+              width: 'auto',
+            },
+          },
         }}
-        onEditorStateChange={onEditorStateChange}
+        onEditorStateChange={(editorState) => {
+          seteditorState(editorState);
+          setcontent(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+      }}
+      blockRendererFn={blockRendererFn}
+        // blockRendererFn={(contentBlock) => {
+        //     const type = contentBlock.getType();
+        //     if (type === 'atomic') {
+        //       return {
+        //         component: AtomicImageComponent,
+        //         editable: false,
+        //       };
+        //     }
+        //     return null;
+        //   }}
       />
-    </>
+    </div>
   );
 }
 
