9f0d7e60592975e2f72175afa55cf7d2facdc415
udokaokoye <leviokoye.com>
2023-08-03 09:14:20 -0400

    fixed verifed image

--- Components/Verified.js
+++ Components/Verified.js
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
-
+import verifiedImage from '../public/images/verified.png'
 function Verified({size=20}) {
   return (
-    <Image src={'/../public/images/verified.png'} width={size} height={size} />
+    <Image src={verifiedImage} width={size} height={size} />
   )
 }
 
