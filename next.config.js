9ee796f282f56f6e22ac09ffa7352eb932bf1147
Levi Okoye <leviokoye@gmail.com>
2023-08-09 09:10:02 -0400

    added next config

---
+++ next.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+
+const API_URL = 'http://localhost/scriblo-server/api'
+// const API_URL = 'http://localhost/scriblo-server/api'
+// const API_URL = 'https://api.myscriblo.com/api'
+const nextConfig = {
+  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scriblo.s3.us-east-2.amazonaws.com",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/api/auth/:slug*",
+        destination: "/authentication/:slug*",
+        permanent: true,
+      },
+      {
+        source: '/api/:slug*',
+        destination: `${API_URL}/:slug*`,
+        permanent: true,
+      },
+    ];
+  },
+  // cssLoaderOptions: {
+  //   modules: true,
+  //   localIdentName: "[local]__[hash:base64:5]",
+  // },
+};
+
+module.exports = nextConfig;
