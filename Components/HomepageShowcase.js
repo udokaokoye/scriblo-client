22d98d7e497cc4ac63aed9acccbd2d219ff88322
Udoka Okoye <leviokoye@gmail.com>
2024-08-04 09:18:33 -0400

    testing develop branch

--- Components/HomepageShowcase.js
+++ Components/HomepageShowcase.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 function HomepageShowcase() {
   return (
     <div className='homepageShowcaseContainer'>
-        <h1 className='showcaseHeading'>Stay Curious.</h1>
+        <h1 className='showcaseHeading'>Stay Curious. (Develop Branch)</h1>
         <div className="showcaseBannerImg"></div>
         <p className='showcaseMessage'> Discover stories, thinking, and expertise from writers on any topic</p>
         <Link href={'/#articles'}><button className='showcaseBtn'>Explore Articles <ArrowDropDownIcon /></button></Link>
